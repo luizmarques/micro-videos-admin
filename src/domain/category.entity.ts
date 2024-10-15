d726c3eb4e82d906eb5e24d14639e4523647a256
luizmarques <luiz_alak@hotmail.com>
2024-10-15 08:35:46 +0000

    first commit

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,35 @@
+
+
+export type CategoryConstructorProps = {
+    category_id?: string;
+    name: string;
+    description?: string | null;
+    is_active?: boolean;
+    created_at?: Date;
+}
+
+export type CategoryCreateCommand = {
+    name: string;
+    description: string | null;
+    is_active: boolean;
+}
+
+export class Category {
+    category_id: string;
+    name: string;
+    description: string | null;
+    is_active: boolean;
+    created_at: Date;
+
+    constructor(props: CategoryConstructorProps) {
+        this.category_id = props.category_id ?? null;
+        this.name= props.name;
+        this.description= props.description ?? null;
+        this.is_active= props.is_active ?? true;
+        this.created_at= props.created_at ?? new Date();
+    }
+
+    static create(props: CategoryConstructorProps): Category {
+        return new Category(props)
+    }
+}
